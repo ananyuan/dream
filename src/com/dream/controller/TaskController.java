4570be10108e6bbac0706db066f00119f1e5cf12
yuananan <yuananan@ruaho.com>
2014-10-22 23:45:28 +0800

    学习 @ResponseBody @RequestBody

--- src/com/dream/controller/TaskController.java
+++ src/com/dream/controller/TaskController.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dream.model.Task;
 import com.dream.service.TaskService;
 
 @Controller
+@RequestMapping("/task")
 public class TaskController {
 	@Autowired
 	private TaskService taskService;
@@ -24,5 +29,23 @@ public class TaskController {
         mav.addObject("allTask",tasks);
         
         return mav;
+    }
+    
+    
+    @RequestMapping(value="/{name}", method = RequestMethod.GET)
+	public @ResponseBody Task getTaskInJSON(@PathVariable String name) {
+ 
+    	Task task = new Task();
+    	task.setTitle("this is title");
+    	task.setDescp("setDescp");
+    	task.setStart(name);
+ 
+		return task;
+ 
+	}
+    
+    @RequestMapping(value="/testRb", method = RequestMethod.GET)
+    public @ResponseBody Task testRb(@RequestBody Task t) {
+        return t;
     }
 }
